716a77d6b2e4d78e25a364e1562271edf81609e3
Pâris DOUADY <paris.douady@hotmail.fr>
2024-11-04 14:47:03 +0100

    deformation fun

--- src/data.rs
+++ src/data.rs
@@ -1,5 +1,15 @@
 use image::RgbaImage;
+use std::f32::consts::{FRAC_PI_2, PI};
 use walkdir::DirEntry;
+
+/// How much to warp width to get orthonormal distances from a tile
+/// Always between [0-1]
+pub fn deformation(z: u32, y: u32) -> f32 {
+    let n_y_tiles = (1 << (z - 1)) as f32;
+    let latitude = FRAC_PI_2 - (y as f32 / n_y_tiles) * PI;
+
+    latitude.cos()
+}
 
 pub fn tile_grad_entries(zoom_levels: &[u32]) -> Vec<DirEntry> {
     print!("reading tile grad entries... {:?}", zoom_levels);
